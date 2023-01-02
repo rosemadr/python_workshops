a13f86d8aa4f10a9ae8452bf0428403de35097ec
Madi Simcock-Brown <m4dele1ne@gmail.com>
2023-01-02 20:09:24 +0000

    added comments to session script

---
+++ session_1.py
@@ -0,0 +1,16 @@
+
+# print("3+3") putting a '#' means this line is a comment, it's contents will be ignored
+# print(3+3) 
+string1 = "Hello Sue" #this is a string
+int1 = 6 # this is an integer
+float1 = 4.3 # this is a float - floating point number/decimal
+bool1 = True # this is a boolean - True or False
+
+# print(type(string1))
+print(3+3)  # because 3 is an integer this will give 6
+print("Hello" + "World")  # because "Hello" and "World" are strings +  will concatenate them to make "HelloWorld" 
+
+# Python is a high level, interpreted and dynamically typed programming language
+# high level means its closer to human language
+# interpreted (rather than compiled)
+# dynamically typed language means python will infer what data type a variable is, you do not have to declare it
